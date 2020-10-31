e49d78517c514ab56dab06a4dabba9a76292d99a
Marlise Borba <borba.marlise@gmail.com>
2020-10-30 23:10:55 -0300

    Estrutura base da api

---
+++ UsuarioApi/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UsuarioApi.Models
+{
+    public class Usuario
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Matricula { get; set; }
+        public string Senha { get; set; }
+        public bool Ativo { get; set; }
+        public string Email { get; set; }
+    }
+}
