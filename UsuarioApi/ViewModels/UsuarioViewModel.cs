16275684b2dbf6261e59233b2350526bdb10ab47
Marlise Borba <borba.marlise@gmail.com>
2020-11-05 05:52:30 -0300

    Mapeamento da view usuario

--- UsuarioApi/ViewModels/UsuarioViewModel.cs
+++ UsuarioApi/ViewModels/UsuarioViewModel.cs
@@ -1,20 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace UsuarioApi.ViewModel
+namespace UsuarioApi.ViewModels
 {
     public class UsuarioViewModel
     {
         public long Id { get; set; }
+        
         [Required]
         public string Nome { get; set; }
         [Required]
         public string Login { get; set; }
         [Required]
         public string Matricula { get; set; }
+        
         [Required]
         public string Senha { get; set; }
         public bool Ativo { get; set; }
