14a434c8360ed4d677447001aacb912d0023e963
Marlise Borba <borba.marlise@gmail.com>
2020-11-05 03:03:39 -0300

    Cria UsuarioViewModel

--- UsuarioApi/ViewModel/UsuarioViewModel.cs
+++ UsuarioApi/ViewModel/UsuarioViewModel.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
-namespace UsuarioApi.Models
+namespace UsuarioApi.ViewModel
 {
-    public class Usuario
+    public class UsuarioViewModel
     {
         public long Id { get; set; }
+        [Required]
         public string Nome { get; set; }
+        [Required]
         public string Login { get; set; }
+        [Required]
         public string Matricula { get; set; }
+        [Required]
         public string Senha { get; set; }
         public bool Ativo { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "E-mail em formato inválido")]
         public string Email { get; set; }
     }
 }
